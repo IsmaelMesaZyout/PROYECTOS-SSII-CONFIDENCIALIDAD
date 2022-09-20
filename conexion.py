288c0bc7d6d47991596fa79e3cf16506d5a4113c
Ismael <ismaelmesazyout@gmail.com>
2022-09-20 18:22:55 +0200

    Pasos avanzados

--- conexion.py
+++ conexion.py
@@ -1,6 +1,7 @@
 from mysql.connector import Error
 import mysql.connector
-import FuncionHash 
+import FuncionHash
+import time 
 try:
     connection = mysql.connector.connect(
         host='localhost',
@@ -23,15 +24,42 @@ except Error as ex:
 
 
 cursor = connection.cursor()
-sql = "INSERT INTO primeratabla(IdHash,NumeroHash) VALUES (%s, %s)"
+sql = "INSERT INTO segundatabla(Nombre,NumeroHash) VALUES (%s, %s)"
+#sql1 = "INSERT INTO segundatabla(Nombre,NumeroHash, ID) VALUES (%s, %s, %s)"
 p = FuncionHash.getmd5file("C:/Users/Ismael/Desktop/horario_provisional.png")
 v = FuncionHash.getmd5file("C:/Users/Ismael/Desktop/new.csv")
 
-val = [
-    (5,p),
-    (6,v)
-]
+lista = []
+c = True
+val =[
+        ("horario_provisional",p),
+        ("new",v)
+    ]
 cursor.executemany(sql,val)
-
-
 connection.commit()
+
+def run_query(query=''): 
+   
+    cursor = connection.cursor()
+    cursor.execute(query)          # Ejecutar una consulta 
+    if query.upper().startswith('SELECT'): 
+        data = cursor.fetchall()   # Traer los resultados de un select 
+    else:               # Hacer efectiva la escritura de datos 
+        data = None
+    return data
+
+
+while(c):
+    time.sleep(5) 
+    val1 = "SELECT * FROM segundatabla" #se saca la lista del sql
+    val1 = run_query(val1)
+    v = FuncionHash.getmd5file("C:/Users/Ismael/Desktop/new.csv")
+    p = FuncionHash.getmd5file("C:/Users/Ismael/Desktop/horario_provisional.png")
+    val =[
+        ("new",v),
+        ("horario_provisional",p)
+    ]
+    if (val != val1):
+        print("Se ha modificado el hash de un archivo")
+    else:
+        print("nothing")
